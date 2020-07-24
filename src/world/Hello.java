602466a7d3f9a55c9b3e3f36d22183daf02dc329
Daniel Tisdale <dctisdal@calpoly.edu>
2020-07-24 22:53:04 +0700

    fixed smells

--- src/world/Hello.java
+++ src/world/Hello.java
@@ -1,12 +1,12 @@
-package hello;
+package world;
 
 import java.util.logging.Logger;
 import java.util.logging.level;
 
-public class Hello {
+public class HelloWorld {
 	public static void main(String[] args) {
 		// prints "Hello World" to terminal window
-		Logger logger = Logger.getLogger(Hello.class.getName());
+		Logger logger = Logger.getLogger(HelloWorld.class.getName());
 		logger.log(Level.INFO, "Hello World!");
 	}
 }
